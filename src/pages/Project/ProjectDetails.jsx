a5d15761216a7ff0b165f94cd590ec192069447f
Eldhose Babu <eldhosebabu316@gmail.com>
2024-03-04 00:06:17 +0530

    changes

--- src/pages/Project/ProjectDetails.jsx
+++ src/pages/Project/ProjectDetails.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useProjectData } from "../Register/ProjectDataContext";
+
 const ProjectDetails = () => {
   const { projectData } = useProjectData();
 
@@ -7,32 +8,34 @@ const ProjectDetails = () => {
     <div className="mt-8">
       <h1 className="text-2xl font-semibold mb-4">Registered Projects</h1>
       {projectData.length > 0 ? (
-        <table className="min-w-full bg-white border border-gray-300">
-          <thead>
-            <tr>
-              <th className="py-2 px-4 border-b">Project Title</th>
-              <th className="py-2 px-4 border-b">From</th>
-              <th className="py-2 px-4 border-b">Due</th>
-              <th className="py-2 px-4 border-b">Team Lead</th>
-              <th className="py-2 px-4 border-b">Employees</th>
-              <th className="py-2 px-4 border-b">Description</th>
-            </tr>
-          </thead>
-          <tbody>
-            {projectData.map((project, index) => (
-              <tr key={index}>
-                <td className="py-2 px-4 border-b">{project.projectTitle}</td>
-                <td className="py-2 px-4 border-b">{project.from}</td>
-                <td className="py-2 px-4 border-b">{project.due}</td>
-                <td className="py-2 px-4 border-b">{project.teamLead.label}</td>
-                <td className="py-2 px-4 border-b">
-                  {project.employees.map((employee) => employee.label).join(", ")}
-                </td>
-                <td className="py-2 px-4 border-b">{project.description}</td>
+        <div className="overflow-x-auto">
+          <table className="min-w-full bg-white rounded-md overflow-hidden shadow-md">
+            <thead className="bg-gray-200">
+              <tr>
+                <th className="py-3 px-4 border-b border-gray-300">Project Title</th>
+                <th className="py-3 px-4 border-b border-gray-300">From</th>
+                <th className="py-3 px-4 border-b border-gray-300">Due</th>
+                <th className="py-3 px-4 border-b border-gray-300">Team Lead</th>
+                <th className="py-3 px-4 border-b border-gray-300">Employees</th>
+                <th className="py-3 px-4 border-b border-gray-300">Description</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody className="divide-y divide-gray-200">
+              {projectData.map((project, index) => (
+                <tr key={index}>
+                  <td className="py-3 px-4 border whitespace-nowrap">{project.projectTitle}</td>
+                  <td className="py-3 px-4 border whitespace-nowrap">{project.from}</td>
+                  <td className="py-3 px-4 border whitespace-nowrap">{project.due}</td>
+                  <td className="py-3 px-4 border whitespace-nowrap">{project.teamLead.label}</td>
+                  <td className="py-3 px-4 border whitespace-nowrap">
+                    {project.employees.map((employee) => employee.label).join(", ")}
+                  </td>
+                  <td className="py-3 px-4 border whitespace-nowrap">{project.description}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       ) : (
         <p>No projects found.</p>
       )}
@@ -40,4 +43,4 @@ const ProjectDetails = () => {
   );
 };
 
-export default ProjectDetails;
+export default ProjectDetails;
--- src/pages/Register/EmpRegister.jsx
+++ src/pages/Register/EmpRegister.jsx
@@ -1,15 +1,15 @@
-import  { useState } from 'react';
+import React, { useState } from 'react';
+import Swal from 'sweetalert2';
 
 const EmpRegister = () => {
- 
   const [employeeData, setEmployeeData] = useState({
     firstName: '',
     lastName: '',
     email: '',
     position: '',
   });
+  console.log(employeeData);
 
-  
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setEmployeeData({
@@ -18,78 +18,90 @@ const EmpRegister = () => {
     });
   };
 
-  
   const handleRegistration = (e) => {
     e.preventDefault();
-   
-    console.log('Employee Registration Data:', employeeData);
+
     
+    localStorage.setItem('employeeData', JSON.stringify(employeeData));
+
+   
     setEmployeeData({
       firstName: '',
       lastName: '',
       email: '',
       position: '',
     });
+
+  
+    Swal.fire({
+      icon: 'success',
+      title: 'Employee Registered Successfully',
+      showConfirmButton: false,
+      timer: 1500
+    });
   };
 
   return (
     <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-lg shadow-md border border-gray-300">
-        <div className="max-w-md w-full p-8 bg-white rounded-lg shadow-lg">
-      <h2 className="text-2xl font-bold text-gray-900 text-center mb-6">Employee Registration</h2>
-      <form onSubmit={handleRegistration} className="space-y-6">
-        <label className="block text-sm font-medium text-gray-900">
-          First Name:
-          <input
-            type="text"
-            name="firstName"
-            value={employeeData.firstName}
-            onChange={handleInputChange}
-            required
-            className="block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </label>
-        <br />
-        <label className="block text-sm font-medium text-gray-900">
-          Last Name:
-          <input
-            type="text"
-            name="lastName"
-            value={employeeData.lastName}
-            onChange={handleInputChange}
-            required
-            className="block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </label>
-        <br />
-        <label className="block text-sm font-medium text-gray-900">
-          Email:
-          <input
-            type="email"
-            name="email"
-            value={employeeData.email}
-            onChange={handleInputChange}
-            required
-            className="block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </label>
-        <br />
-        <label className="block text-sm font-medium text-gray-900">
-          Position:
-          <input
-            type="text"
-            name="position"
-            value={employeeData.position}
-            onChange={handleInputChange}
-            required
-            className="block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-          />
-        </label>
-        <br />
-        <button type="submit" className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
->
-            Register Employee</button>
-      </form>
-    </div>
+      <div className="max-w-md w-full p-8 bg-white rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold text-gray-900 text-center mb-6">Employee Registration</h2>
+        <form onSubmit={handleRegistration} className="space-y-6">
+          <label className="block text-sm font-medium text-gray-900">
+            First Name:
+            <input
+              type="text"
+              name="firstName"
+              value={employeeData.firstName}
+              onChange={handleInputChange}
+              required
+              className="block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </label>
+          <br />
+          <label className="block text-sm font-medium text-gray-900">
+            Last Name:
+            <input
+              type="text"
+              name="lastName"
+              value={employeeData.lastName}
+              onChange={handleInputChange}
+              required
+              className="block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </label>
+          <br />
+          <label className="block text-sm font-medium text-gray-900">
+            Email:
+            <input
+              type="email"
+              name="email"
+              value={employeeData.email}
+              onChange={handleInputChange}
+              required
+              className="block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </label>
+          <br />
+          <label className="block text-sm font-medium text-gray-900">
+            Position:
+            <input
+              type="text"
+              name="position"
+              value={employeeData.position}
+              onChange={handleInputChange}
+              required
+              className="block w-full pl-3 pr-3 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+            />
+          </label>
+          <br />
+          <button
+            type="submit"
+            className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Register Employee
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
