57f91ea6b89abc8086ae2e8ad4c4d001963e8768
Eldhose Babu <eldhosebabu316@gmail.com>
2024-03-09 13:42:13 +0530

    project-listings-api

--- src/pages/Project/ProjectDetails.jsx
+++ src/pages/Project/ProjectDetails.jsx
@@ -12,7 +12,7 @@ const ProjectDetails = () => {
   useEffect(() => {
     const fetchProjectDetails = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8001/hrapi/projects/', {
+        const response = await axios.get('http://127.0.0.1:8001/teamleadapi/projects/', {
           headers: {
             'Authorization': `Token ${token}`, 
           }
@@ -28,37 +28,35 @@ const ProjectDetails = () => {
   }, []);
 
   return (
-    <div className="mt-8">
-      <h1 className="text-2xl font-semibold mb-4">Registered Projects</h1>
-      {projectData.length > 0 ? (
-        <div className="overflow-x-auto overflow-y-auto">
-          <table className="min-w-full bg-white rounded-md overflow-scroll shadow-md">
-            <thead className="bg-gray-200">
-              <tr>
-                <th className="py-3 px-4 border-b border-gray-300">Project Title</th>
-                <th className="py-3 px-4 border-b border-gray-300">Project Description</th>
-                <th className="py-3 px-4 border-b border-gray-300">Due Date</th>
-                <th className="py-3 px-4 border-b border-gray-300">Project Status</th>
-              
+    <div className="mt-8 h-96 overflow-y-auto">
+    <h1 className="text-2xl font-semibold mb-4">Registered Projects</h1>
+    {projectData.length > 0 ? (
+      <div className="overflow-x-auto">
+        <table className="min-w-full bg-white rounded-md shadow-md">
+          <thead className="bg-gray-200">
+            <tr>
+              <th className="py-3 px-4 border-b border-gray-300">Project Title</th>
+              <th className="py-3 px-4 border-b border-gray-300">Project Description</th>
+              <th className="py-3 px-4 border-b border-gray-300">Due Date</th>
+              <th className="py-3 px-4 border-b border-gray-300">Project Status</th>
+            </tr>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {projectData.map((project, index) => (
+              <tr key={index}>
+                <td className="py-3 px-4 border whitespace-nowrap">{project.topic}</td>
+                <td className="py-3 px-4 border whitespace-nowrap">{project.description}</td>
+                <td className="py-3 px-4 border whitespace-nowrap">{project.end_date}</td>
               </tr>
-            </thead>
-            <tbody className="divide-y divide-gray-200">
-              {projectData.map((project, index) => (
-          
-                <tr key={index}>
-                  <td className="py-3 px-4 border whitespace-nowrap">{project.topic}</td>
-                  <td className="py-3 px-4 border whitespace-nowrap">{project.description}</td>
-                  <td className="py-3 px-4 border whitespace-nowrap">{project.end_date}</td>
-                  
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      ) : (
-        <p>No projects found.</p>
-      )}
-    </div>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    ) : (
+      <p className="mt-4">No projects found.</p>
+    )}
+  </div>
+  
   );
 };
 
