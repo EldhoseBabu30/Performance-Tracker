743b9039bedd977a973baeebda9af2fc9b065ce0
mohamed-jamsheed <mohamedjamsheed50@gmail.com>
2024-03-17 19:34:16 +0530

    emp

--- src/pages/Emp-Home/EmployeeHome.jsx
+++ src/pages/Emp-Home/EmployeeHome.jsx
@@ -1,9 +1,202 @@
-import React from 'react'
+import React,{useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
+import {
+  Card,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  InboxIcon,
+  PowerIcon,
+  EyeIcon,
+} from "@heroicons/react/24/solid";
 
 const EmployeeHome = () => {
+
+  const name = localStorage.getItem("userName");
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const navigate = useNavigate();
+
+  
+  const handleRegisterClick = () => {
+    navigate("");
+  };
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+
   return (
-    <div>
-      
+    <div className="flex flex-row h-screen">
+      {/* Toggle button for sidebar */}
+      <button onClick={toggleSidebar} className="text-gray-500 fixed top-4 left-4 z-50">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
+
+      {/* Sidebar */}
+      <Card
+        className={`w-64 p-4 shadow-xl shadow-blue-gray-900/5 ${isSidebarOpen ? "block" : "hidden"}`}
+      >
+        <div className="mb-2 p-4">
+          <Typography variant="h5" color="blue-gray">Sidebar Content</Typography>
+        </div>
+        <List>
+          <ListItem>
+            <ListItemPrefix>
+              <PresentationChartBarIcon className="h-5 w-5" />
+            </ListItemPrefix>
+            Dashboard
+          </ListItem>
+          <Link to='/hr-project-details'>
+            <ListItem>
+              <ListItemPrefix>
+                <EyeIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              View Project Details
+            </ListItem>
+          </Link>
+          <Link to='/view-teams'>
+            <ListItem>
+              <ListItemPrefix>
+                <EyeIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              View Teams
+            </ListItem>
+          </Link>
+          <Link to='/assigned-projects'>
+            <ListItem>
+              <ListItemPrefix>
+                <EyeIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              Assigned Projects
+            </ListItem>
+          </Link>
+          
+            <ListItem >
+              <ListItemPrefix>
+                <InboxIcon className="h-5 w-5" />
+              </ListItemPrefix>
+              Inbox
+              <ListItemSuffix>
+                <Chip
+                  // value={requestsCount} // Pass the count of requests
+                  size="sm"
+                  variant="ghost"
+                  color="blue-gray"
+                  className="flex items-center justify-center rounded-full w-6 h-6 bg-red-500 text-white text-xs mx-1"
+                />
+              </ListItemSuffix>
+            </ListItem>
+          <ListItem>
+            <ListItemPrefix>
+              <PowerIcon className="h-5 w-5" />
+            </ListItemPrefix>
+            Log Out
+          </ListItem>
+        </List>
+      </Card>
+
+      {/* Content */}
+      <div className="flex flex-col flex-grow">
+        <div className="flex justify-center items-center h-1/6">
+          <h3 className="text-3xl font-medium leading-tight text-center text-primary">
+            Welcome, {name}
+          </h3>
+        </div>
+
+        {/* Cards container */}
+        <div className="flex justify-center items-start h-5/6 px-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full px-4">
+            {/* Card 1 */}
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Project
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <Link to="/register-project">
+                  <button
+                    type="button"
+                    className="mt-4 bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 text-white font-medium rounded-lg text-sm flex items-center px-6 py-2.5"
+                  >
+                    <span>Register</span>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth={1.5}
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                      />
+                    </svg>
+                  </button>
+                </Link>
+              </div>
+            </div>
+
+            {/* Card 2 */}
+            <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+              <div className="p-6">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                  Projects
+                </h5>
+                <p className="font-normal text-gray-700 dark:text-gray-400">
+                  Here are the biggest enterprise technology acquisitions of
+                  2021 so far, in reverse chronological order.
+                </p>
+                <button
+                  type="button"
+                  onClick={handleRegisterClick}
+                  className="mt-4 bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 text-white font-medium rounded-lg text-sm flex items-center px-6 py-2.5"
+                >
+                  <span>Register</span>
+
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M10.125 2.25h-4.5c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125v-9M10.125 2.25h.375a9 9 0 0 1 9 9v.375M10.125 2.25A3.375 3.375 0 0 1 13.5 5.625v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 0 1 3.375 3.375M9 15l2.25 2.25L15 12"
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
